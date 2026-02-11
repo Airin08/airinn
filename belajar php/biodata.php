658394dfcb373d859808cdd5d8e04367441daa7e
Airin08 <fitriainayah001@gmail.com>
2026-02-11 10:27:01 +0700

    update.php

---
+++ belajar php/biodata.php
@@ -0,0 +1,4 @@
+<?php
+echo "Nama : Airin Fitria Inayah <br>";
+echo "Kelas : XI TJKT 4 <br>";
+echo "Tempat, Tanggal Lahir : Bandung, 04 oktober 2008 <br>";
---
+++ belajar php/variabel.php
@@ -0,0 +1,14 @@
+<?php
+
+// tipe data char (karakter)
+$jenis_kelamin = 'Perempuan';
+
+// tipe data sharing (teks)
+$nama_lengkap = "Airin Fitria Inayah";
+
+// tipe data integer
+$umur = 17;
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
